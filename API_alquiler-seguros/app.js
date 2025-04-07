05c51e3766c0eddc6af1fee636dec5734387c09b
Samuel Cobas <samu.cobas.ort@gmail.com>
2025-04-07 20:41:34 +0200

    creacion directorios, configurar .env, dependencias express y mysql2 y estructura inicial

---
+++ API_alquiler-seguros/app.js
@@ -0,0 +1,20 @@
+// Dependencias
+import express from "express";
+
+// Variables de entorno
+import { PORT } from "./env.js";
+
+// Se crea el servidor
+const app = express();
+
+// Middleware que "desencripta" un body en formato "raw" creando la propiedad "body" en el objeto "request".
+app.use(express.json());
+
+// Inicio (/), Sin esto da error por que intenta cargar la ruta / igualmente, aunque no esté definida
+app.use("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
