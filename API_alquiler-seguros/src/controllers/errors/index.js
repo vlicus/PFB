40bd8556931a15d381f71139817fbb162fc3441a
Samuel Cobas <samu.cobas.ort@gmail.com>
2025-04-08 21:01:29 +0200

    add front directory

--- API_alquiler-seguros/src/controllers/errors/index.js
+++
@@ -1,4 +0,0 @@
-import errorController from "./errorController.js";
-import notFoundController from "./notFoundController.js";
-
-export { errorController, notFoundController };
--- API_alquiler-seguros/src/controllers/errors/notFoundController.js
+++
@@ -1,7 +0,0 @@
-import { notFoundError } from "../../services/errorService.js";
-
-const notFound = (req, res, next) => {
-  next(notFoundError("ruta"));
-};
-
-export default notFound;
