ec3e14e227534b351a5ff25c6b33095751b2ee9c
Nicolas Fernandez <nicofernandezdel@gmail.com>
2025-04-08 19:59:04 +0200

    añadimos los middlewares de errores y error 404

--- API_alquiler-seguros/app.js
+++ API_alquiler-seguros/app.js
@@ -3,7 +3,11 @@ import express from "express";
 import cors from "cors";
 // Variables de entorno
 import { PORT } from "./env.js";
-
+//Importamos los errores
+import {
+  notFoundController,
+  errorController,
+} from "./src/controllers/errors/index.js";
 // Se crea el servidor
 const app = express();
 
@@ -15,8 +19,14 @@ app.use("/", (req, res) => {
   res.send("Hello, world!");
 });
 
+//Middleware de ruta no encontrada
+app.use(notFoundController);
+
+//Middleware de errores
+app.use(errorController);
+
+app.use(cors());
+
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
-
-app.use(cors());
