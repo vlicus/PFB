50eddac43a9c4d8fcc001ea5d35ed6e2aeaab2f3
Samuel Cobas <samu.cobas.ort@gmail.com>
2025-04-08 20:27:31 +0200

    terminado newUserController e importacion a app.js desde userRoutes.js

--- API_alquiler-seguros/app.js
+++ API_alquiler-seguros/app.js
@@ -4,11 +4,17 @@ import express from "express";
 // Variables de entorno
 import { PORT } from "./env.js";
 
+// Importamos las rutas
+import userRoutes from "./src/routes/userRoutes.js";
+
 // Se crea el servidor
 const app = express();
 
 // Middleware que "desencripta" un body en formato "raw" creando la propiedad "body" en el objeto "request".
 app.use(express.json());
+
+// Middleware que indica a express donde están las rutas
+app.use(userRoutes);
 
 // Inicio (/), Sin esto da error por que intenta cargar la ruta / igualmente, aunque no esté definida
 app.use("/", (req, res) => {
---
+++ API_alquiler-seguros/src/controllers/users/newUserController.js
@@ -0,0 +1,38 @@
+import randomstring from "randomstring";
+
+import insertUserModel from "../../models/users/insertUserModel";
+
+// TBD, Esquema de Joi (Extra)
+/* import validateSchemaUtil from "../../utils/validateSchemaUtil.js"
+ */
+
+// TBD, Importamos el esquema
+/* import newUserSchema from "../../schemas/users/newUserSchema.js" */
+
+// Función controladora final que crea un nuevo usuario.
+const newUserController = async (req, res, next) => {
+  try {
+    // Obtenemos los datos necesarios del body
+    const { username, email, password } = req.body;
+
+    // Validamos el body con Joi
+    await validateSchemaUtil(newUserSchema, req.body);
+
+    // Creamos el código de registro.
+    const registration_code = randomstring.generate(30);
+
+    // Insertamos el usuario
+    await insertUserModel(username, email, password, registration_code);
+
+    res.send({
+      status: "ok",
+      message: `
+            Usuario creado.
+            `,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default newUserController;
