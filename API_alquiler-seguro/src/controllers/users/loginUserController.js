0ffcba5da8cf9861e705b7e0f0b43c9002a71c5d
Samuel Cobas <samu.cobas.ort@gmail.com>
2025-04-09 17:37:51 +0200

    Login Postman success

--- API_alquiler-seguro/src/controllers/users/loginUserController.js
+++ API_alquiler-seguro/src/controllers/users/loginUserController.js
@@ -1,48 +1,49 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken'
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-import getPool from '../../db/getPool';
-import generateErrorUtil from '../../utils/generateErrorUtil';
+import getPool from "../../db/getPool.js";
+import generateErrorUtil from "../../utils/generateErrorUtil.js";
 
-const loginUserController= async (req,res,next)=>{
-      try{
-            const {email, password}= req.body;
-            if(!email || !password){
-                  generateErrorUtil('faltan campos', 400)
-            }
-            
-            const pool = await getPool(); 
-            const [users]=await pool.query(
-                  'SELECT id, password, active FROM users WHERE email = ?', //revisar la query si está bien
-                  email,
-            );
-            const validPass= 
-            users.length>0 && (await bcrypt.compare(password, users[0].password));
+const loginUserController = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      generateErrorUtil("faltan campos", 400);
+    }
 
-            if (!validPass){
-                  generateErrorUtil('Credenciales inválidas', 401);
-            }
-            if(!users[0].active){
-                  generateErrorUtil(
-                        'Usuario pendiente de activar. Activa tu usuario accediendo al email de verificación que has recibido en tu correo',
-                  403,);   
-            }
-            const tokenInfo={
-                  id: users[0].id,
-                  role: users[0].role,
-            };
-            const token = jwt.sign(tokenInfo, process.env.SECRET, {
-                  expiresIn: '7d',
-            });
-            res.send({
-                  status: 'ok',
-                  data: {
-                        token,
-                  },
-            });
-      }catch(err){
-            next(err);
-      }
+    const pool = await getPool();
+    const [users] = await pool.query(
+      "SELECT id, password, active FROM users WHERE email = ?", //revisar la query si está bien
+      email
+    );
+    const validPass =
+      users.length > 0 && (await bcrypt.compare(password, users[0].password));
+
+    if (!validPass) {
+      generateErrorUtil("Credenciales inválidas", 401);
+    }
+    if (!users[0].active) {
+      generateErrorUtil(
+        "Usuario pendiente de activar. Activa tu usuario accediendo al email de verificación que has recibido en tu correo",
+        403
+      );
+    }
+    const tokenInfo = {
+      id: users[0].id,
+      role: users[0].role,
+    };
+    const token = jwt.sign(tokenInfo, process.env.SECRET, {
+      expiresIn: "7d",
+    });
+    res.send({
+      status: "ok",
+      data: {
+        token,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default loginUserController;
