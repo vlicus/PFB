5da2b617ecba9a40356e6919c958b2819430b754
carlos 'tuto' curiel <ccurielramos@gmail.com>
2025-04-14 18:37:43 +0200

    creado el endpoint de crear alquiler, modificada tabla de rent a rents.

--- API_alquiler-seguro/src/routes/rentRoutes.js
+++ API_alquiler-seguro/src/routes/rentRoutes.js
@@ -6,6 +6,7 @@ import {
   addRentPhotoController,
   deleteRentPhotoController,
   voteRentController,
+  newRentController,
 } from "../controllers/rents/index.js";
 
 import {
@@ -27,6 +28,8 @@ router.post(
   canEditController,
   addRentPhotoController
 );
+// Insertar un nuevo alquiler.
+router.post("/rent/register", authUserController, newRentController);
 
 // Eliminar una foto de una entrada.
 router.delete(
