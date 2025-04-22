ae7d7e6c652b867a85061c240651cc26c0a8ce15
Samuel Cobas <samu.cobas.ort@gmail.com>
2025-04-22 21:25:11 +0200

    C:/Program Files/Git/register/rent form creado

--- FRONT_alquiler-seguro/src/App.jsx
+++ FRONT_alquiler-seguro/src/App.jsx
@@ -6,6 +6,7 @@ import Register from "./pages/Register";
 import Header from "./components/Header";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
+import NewRentForm from "./pages/NewRentForm";
 
 import "./index.css";
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
         <Route path="/register" element={!token ? <Register /> : <Navigate to="/" />} />
         <Route path="login" element={!token ? <Login /> : <Navigate to="/" />} />
         <Route path="/profile" element={token ? <Profile /> : <Navigate to="/login" />} />
+        <Route path="/rent/register" element={token ? <NewRentForm /> : <Navigate to="/login" />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
