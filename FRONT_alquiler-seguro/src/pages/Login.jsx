078478c2364155893a6d7be4133d83d6f71c9ea4
Samuel Cobas <samu.cobas.ort@gmail.com>
2025-05-06 01:25:45 +0200

    pequeños ajustes de css

--- FRONT_alquiler-seguro/src/pages/Login.jsx
+++ FRONT_alquiler-seguro/src/pages/Login.jsx
@@ -5,7 +5,6 @@ import useSendRecoveryEmail from "../hooks/useSendRecoveryEmail";
 
 import "../styles/Login-Register.css";
 
-
 export default function Login() {
   const { formState, handleSubmit, handleChange } = useLogin();
 
@@ -14,36 +13,36 @@ export default function Login() {
   return (
     <main>
       <div className="form-card">
-         <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          <li>
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              required
-              id="email"
-              name="email"
-              value={formState.email}
-              onChange={handleChange}
-            />
-          </li>
-          <li>
-            <label htmlFor="password">Contraseña</label>
-            <PasswordInput
-              required
-              id="password"
-              name="password"
-              value={formState.password}
-              onChange={handleChange}
-            />
-          </li>
-        </ul>
-        <button>Log in</button>
-      </form>
-      <form onSubmit={handleRecoverySubmit}>
-        <button>Recuperar Contraseña</button>
-      </form>
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <ul>
+            <li>
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                required
+                id="email"
+                name="email"
+                value={formState.email}
+                onChange={handleChange}
+              />
+            </li>
+            <li>
+              <label htmlFor="password">Contraseña</label>
+              <PasswordInput
+                required
+                id="password"
+                name="password"
+                value={formState.password}
+                onChange={handleChange}
+              />
+            </li>
+          </ul>
+          <button>Log in</button>
+        </form>
+        <form onSubmit={handleRecoverySubmit}>
+          <button>Recuperar Contraseña</button>
+        </form>
       </div>
     </main>
   );
--- FRONT_alquiler-seguro/src/pages/SendEmailPasswordRecovery.jsx
+++ FRONT_alquiler-seguro/src/pages/SendEmailPasswordRecovery.jsx
@@ -1,23 +1,26 @@
 import useSendRecoveryEmail from "../hooks/useSendRecoveryEmail";
+import "../styles/Login-Register.css";
 
 export default function SendEmailPasswordRecovery() {
   const { handleRecoveryEmailSubmit, handleRecoveryChange, recoveryEmailState } =
     useSendRecoveryEmail();
   return (
     <main>
-      <h2>Introduzca el email para recuperar su contraseña</h2>
+      <div className="form-card">
+        <h2>Introduzca el email para recuperar su contraseña</h2>
 
-      <form onSubmit={handleRecoveryEmailSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input
-          name="email"
-          id="email"
-          value={recoveryEmailState.email}
-          onChange={handleRecoveryChange}
-          placeholder="Introduce tu email"
-        />
-        <button>Enviar</button>
-      </form>
+        <form onSubmit={handleRecoveryEmailSubmit}>
+          <label htmlFor="email">Email:</label>
+          <input
+            name="email"
+            id="email"
+            value={recoveryEmailState.email}
+            onChange={handleRecoveryChange}
+            placeholder="Introduce tu email"
+          />
+          <button>Enviar</button>
+        </form>
+      </div>
     </main>
   );
 }
